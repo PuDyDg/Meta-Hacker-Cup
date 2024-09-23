872997247fc0fdca3289683693cbe2b395d4b159
PuDyDg <101185667+PuDyDg@users.noreply.github.com>
2024-09-23 13:30:59 +0700

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -2,34 +2,25 @@
 #define int long long
 using namespace std;
 
-int n, k;
-int a[200005];
-
-int sum(int n) {
-    return n * (n + 1) / 2;
-}
-int sum(int l, int r) {
-
-    return sum(r) - sum(l-1);
-}
 signed main() {
 
-    cin >> n >> k;
-        for (int i = 1 ; i <= n ; i++)
-        cin >> a[i];
-    if (n == 1) {
-        cout << sum(max(1LL, a[1] - k+1), a[1]);
-        return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+
+    int test;
+    cin >> test;
+    for (int num = 1 ; num <= test ; num++) {
+        int n, k, mi = 1e9;
+        cin >> n >> k;
+        for (int i = 1 ; i <= n ; i++) {
+            int a;
+            cin >> a;
+            mi = min(mi, a);
+        }
+        cout << "Case #" << num << ": "; 
+        mi *= max(0LL, (n-2) * 2) + 1;
+        if (mi > k) cout << "NO\n";
+        else cout << "YES\n";
     }
-
-
-    sort(a+1, a+n+1, greater <int> ());
-
-    int T = a[1] - a[2] + 1;
-    int res = k / T * a[2];
-    res += k / T * sum(a[2]+1, a[1]);
-    for (int i = 1 ; i <= k % T ; i++)
-        res += a[1] - i + 1;
-    cout << res;
     return 0;
 }
